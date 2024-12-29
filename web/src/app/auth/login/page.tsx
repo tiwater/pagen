97f5119e0780cc1fedf4ccbe2143abb4f1ff8225
Hugh Lyu <hugh.lv@gmail.com>
2024-12-29 12:36:44 +0800

    chore: login page styles

--- web/src/app/auth/login/page.tsx
+++ web/src/app/auth/login/page.tsx
@@ -1,28 +1,31 @@
-"use client";
+'use client';
 
-import { Icons } from "@/components/icons";
-import { UserAuthForm } from "@/components/user-auth-form";
-import Link from "next/link";
-import { useSearchParams } from "next/navigation";
-import { Suspense } from "react";
+import { Suspense } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
+import { UserAuthForm } from '@/components/user-auth-form';
 
 function LoginContent() {
   const searchParams = useSearchParams();
-  const redirect = searchParams.get("redirect") || "/";
+  const redirect = searchParams.get('redirect') || '/';
 
   return (
-    <div className="container flex h-[calc(100vh-var(--navbar-height))] w-screen flex-col items-center justify-center">
+    <div className="mx-auto container flex h-screen w-screen flex-col items-center justify-center">
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-        <div className="flex flex-col space-y-2 text-center">
-          <Icons.logo className="mx-auto h-20 w-20" />
+        <div className="flex flex-col gap-4 text-center">
+          <Image
+            alt="logo"
+            width={48}
+            height={48}
+            src="/images/logo.svg"
+            className="mx-auto h-20 w-20"
+          />
           <h1 className="text-2xl font-semibold tracking-tight">欢迎回来</h1>
         </div>
         <UserAuthForm redirect={redirect} />
         <p className="px-8 text-center text-sm text-muted-foreground">
-          <Link
-            href="/auth/register"
-            className="hover:text-brand underline underline-offset-4"
-          >
+          <Link href="/auth/register" className="hover:text-brand underline underline-offset-4">
             还没有账号？立即注册
           </Link>
         </p>
--- web/src/app/auth/register/page.tsx
+++ web/src/app/auth/register/page.tsx
@@ -1,24 +1,29 @@
 "use client";
-import { UserAuthForm } from "@/components/user-auth-form";
-import Link from "next/link";
+
+import Image from 'next/image';
+import Link from 'next/link';
+import { UserAuthForm } from '@/components/user-auth-form';
+
 
 export default function RegisterPage() {
   return (
-    <div className="container flex h-[calc(100vh-var(--navbar-height))] w-screen flex-col items-center justify-center">
-      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+    <div className="mx-auto container flex h-screen w-screen flex-col items-center justify-center">
+      <div className="flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-          <div className="flex flex-col space-y-2 text-center">
+          <div className="flex flex-col gap-4 text-center">
+            <Image
+              alt="logo"
+              width={48}
+              height={48}
+              src="/images/logo.svg"
+              className="mx-auto h-20 w-20"
+            />
             <h1 className="text-2xl font-semibold tracking-tight">创建账户</h1>
-            <p className="text-sm text-muted-foreground">
-              使用您的 Google 账户注册
-            </p>
+            <p className="text-sm text-muted-foreground">使用您的 Google 账户注册</p>
           </div>
           <UserAuthForm isSignUp />
           <p className="px-8 text-center text-sm text-muted-foreground">
-            <Link
-              href="/auth/login"
-              className="hover:text-brand underline underline-offset-4"
-            >
+            <Link href="/auth/login" className="hover:text-brand underline underline-offset-4">
               已有账户？立即登录
             </Link>
           </p>
