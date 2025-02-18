cbbbc49d0f099301ac60efe179f3124531c9e2db
Hugh Lyu <hugh.lv@gmail.com>
2025-02-17 18:57:37 -0800

    ci: removed workspace

--- web/src/components/auth-button.tsx
+++ web/src/components/auth-button.tsx
@@ -91,6 +91,11 @@ export function AuthButton() {
             API Documentation
           </Link>
         </DropdownMenuItem>
+        <DropdownMenuItem className="w-full p-2">
+          <Link href="/docs" className="flex w-full items-center gap-2 cursor-pointer">
+            Webshot API
+          </Link>
+        </DropdownMenuItem>
         <DropdownMenuSeparator />
         <DropdownMenuItem
           onClick={() => signOut()}
